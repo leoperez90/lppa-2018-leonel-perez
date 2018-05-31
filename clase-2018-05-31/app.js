58807e2ea6687bebc0eb335082da3cc28ff11cdc
Leonel Perez <leoperez8038@gmail.com>
2018-05-31 12:02:38 -0300

    now the background its changing

--- clase-2018-05-31/app.js
+++ clase-2018-05-31/app.js
@@ -1,1 +1,14 @@
 console.log("hola");
+var changeColor = function(li){
+ li.style.backgroundColor = '#f00'
+}
+
+window.onload = function () {
+  var btnPlay = document.getElementById('play');
+  btnPlay.onclick = function(){
+    var liAlives = document.getElementsByClassName('alive')
+    for (var i = 0; i < liAlives.length; i++) {
+    changeColor( liAlives [i]);
+}
+  }
+}
