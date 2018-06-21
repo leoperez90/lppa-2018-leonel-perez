ca8ba739c7616c2f863123e64db645f9ee8cd831
leoperez90 <leoperez8038@gmail.com>
2018-06-21 11:20:47 -0300

    apply rules for next board(wip)

---
+++ clase-2018-06-14/js/board.js
@@ -0,0 +1,28 @@
+var Board = {
+  rows: 5,
+  columns: 5,
+  getBoard: function() {
+    return document.getElementsByClassName('board')[0];
+  },
+  getCells: function() {
+    var board = Board.getBoard();
+    return board.getElementsByTagName('li');
+  },
+  getAliveCells : function() {
+    return document.getElementsByClassName('alive');
+  },
+  createCells: function() {
+    var board = Board.getBoard();
+    var html = '';
+    for (var i = 0; i < (Board.rows * Board.columns); i++) {
+      html += '<li></li>';
+    }
+    board.innerHTML = html;
+  },
+  nextStep: function() {
+    var boardHTML = Board.getCells();
+    var currentBoard = GameOfLife.getBoardFromHTML(boardHTML, Board.columns);
+    var newBoard = GameOfLife.createEmptyBoard(Board.rows,Board.columns)
+    GameOfLife.getNextStep(currentBoard,newBoard);
+  }
+}
---
+++ clase-2018-06-14/js/game-of-life.js
@@ -0,0 +1,40 @@
+var GameOfLife = {
+  createEmptyBoard:function(rows,cols) {
+  var newBoard =[];
+  for (var i = 0; i < rows; i++) {
+    var row = [];
+    for(var j = 0; j < cols; j++) {
+      row.push(false);
+    }
+    newBoard.push(row);
+   }
+   return newBoard;
+ },
+  getBoardFromHTML: function(cells, cols) {
+    var board = [];
+    var row = [];
+    for(var i = 0; i < cells.length; i++) {
+      var cell = cells[i];
+      var isAlive = cell.className === 'alive';
+      row.push(isAlive);
+      if(i % cols === (cols-1)) {
+        board.push(row);
+        row = [];
+      }
+    }
+    return board;
+  },
+  getNextStep: function(a, b){
+    for (var i = 0; i < a.length; i++) {
+      var row = a[i];
+      for (var j = 0; j < row.length; j++) {
+        var sell = row[j];
+        var willBeAlive = GameOfLife.applyRule(a, i, j)
+      }
+    }
+  },
+  applyRule: function(board, posX, posY){
+    console.log(board, posX, posY);
+    return true;
+  }
+};
