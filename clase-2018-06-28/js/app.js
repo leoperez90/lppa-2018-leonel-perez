a6284ee71ea2dd55fd403679f931f7ccc6dfeb92
leoperez90 <leoperez8038@gmail.com>
2018-06-28 09:48:55 -0300

    apply rulez

---
+++ clase-2018-06-28/js/app.js
@@ -0,0 +1,16 @@
+console.log("4");
+
+var play = function(){
+  var liAlives = Board.getCells();
+  Board.updateCells(liAlives);
+}
+
+// Se ejecuta luego de que se cargue todo el html
+var init = function() {
+  Game.init();
+  Game.start();
+  // var btnPlay = Board.getPlayButton();
+  // btnPlay.onclick = play;
+}
+
+window.onload = init;
