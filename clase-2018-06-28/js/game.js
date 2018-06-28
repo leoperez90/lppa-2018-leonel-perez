23bc3b7e98083ce5378da7eb2d201570ba673c24
leoperez90 <leoperez8038@gmail.com>
2018-06-28 10:04:40 -0300

    play button

--- clase-2018-06-28/js/game.js
+++ clase-2018-06-28/js/game.js
@@ -7,14 +7,16 @@ var Game = {
     Game.board = Board.getBoard();
     Game.cells = Board.getCells();
     Game.next = document.getElementById('next');
+    Game.play = document.getElementById('play')
   },
   start: function() {
     for (var i = 0; i < Game.cells.length; i++) {
       Game.cells[i].onclick = Cell.toggle;
     }
     Game.next.onclick = Board.nextStep;
+    Game.play.onclick = Game.update;
   },
   update: function() {
-
+    setInterval(Board.nextStep,1000);
   }
 };
